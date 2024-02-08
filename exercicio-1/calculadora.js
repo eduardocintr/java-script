e284be13f8b635ec81442f8578a37be218bb71d6
Eduardo Cintra <82892813+eduardocintr@users.noreply.github.com>
2024-02-08 00:35:24 -0300

    Primeira do ano

---
+++ exercicio-1/calculadora.js
@@ -0,0 +1,14 @@
+var x = Number(prompt("Informe o primeiro número:"))
+var y = Number(prompt("Informe o segundo número:"))
+
+var soma = y + x
+var subtracao = x - y
+var multiplicacao = x * y
+var divisao = x / y
+
+alert(`Resultados:\n
+Soma: ${soma}
+\nSubtração: ${subtracao}
+\nMultiplicação: ${multiplicacao}
+\nDivisão: ${divisao}`
+)
